fa85a82572632ce4ecfd599654d6b9b2266f37f2
inexasli <admin@inexasli.com>
2025-03-19 17:43:58 -0700

    SWITCH, Checklist buttons

--- server/scripts/hideShow.js
+++ server/scripts/hideShow.js
@@ -1,10 +1,12 @@
+import { getCookie } from '/server/scripts/getcookie.js';
+
 function hideShowClass(className, task) {
     const elements = document.getElementsByClassName(className);
     Array.from(elements).forEach((element) => {
         if (element) {
             if (task === 'hide') {
                 element.style.display = 'none';
-            } else if (task ==='show') {
+            } else if (task === 'show') {
                 element.style.display = 'block';
             }
         } else {
@@ -14,36 +16,14 @@ function hideShowClass(className, task) {
     });
 }
 
- function getCook1(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-        // Decode the cookie value
-        const decodedValue = decodeURIComponent(parts.pop().split(';').shift());
-        // If the value is empty and it's a frequency field, set it to 'annually'
-        if (decodedValue === '' && name.includes('_frequency')) {
-            return 'annually';
-        }
-        return decodedValue == 0 || decodedValue == '0'? '': decodedValue;
-    } else {
-        return 'annually';
-    }
-}
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
+    const region = getCookie('RegionDropdown');
 
-    
-    const region = getCook1('RegionDropdown')
-
-if (region == 'CAN') {
-    hideShowClass('usa-hide', 'hide')
-    hideShowClass('can-hide', 'show')
-
-} else if (region == 'USA') {
-    hideShowClass('usa-hide', 'show')
-    hideShowClass('can-hide', "hide")
-
-}
-})
+    if (region === 'CAN') {
+        hideShowClass('usa-hide', 'hide');
+        hideShowClass('can-hide', 'show');
+    } else if (region === 'USA') {
+        hideShowClass('usa-hide', 'show');
+        hideShowClass('can-hide', 'hide');
+    }
+});
