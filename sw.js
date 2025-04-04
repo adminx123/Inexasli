0384bcb54d8f1f43a2ed840e36a6f60e5622360a
inexasli <admin@inexasli.com>
2025-04-04 16:03:18 -0700

    a

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inexasli-v6'; //#CHANGEVERSION with each major update
+const CACHE_NAME = 'inexasli-v7'; //#CHANGEVERSION with each major update
 const urlsToCache = [
   '/budget/index.html'
 ];
