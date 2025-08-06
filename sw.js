d579fc44d46375c068480556159c2e0853385fcd
inexasli <admin@inexasli.com>
2025-08-05 19:41:18 -0700

    fix(serviceWorker): revert cache version to v1 for consistency

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inexasli-v15'; //#CHANGEVERSION with each major update
+const CACHE_NAME = 'inexasli-v1'; //#CHANGEVERSION with each major update
 const urlsToCache = [
   '/budget/index.html'
 ];
